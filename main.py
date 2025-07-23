c4bdb6d18cde26ea2ab677ee0401391026e381aa
nyyanja <nyanja.ramahavatra@gmail>
2025-07-24 06:21:39 +1100

    Feat : Add fourth  question, it's /students

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import List, Optional
 
 app = FastAPI()
 
@@ -11,3 +13,15 @@ def hello():
 @app.get("/welcome",status-code = 200)
 def welcome(name: str):
     return f"Welcome {name}"
+
+#question 3
+class Student(BaseModel):
+    Reference: str
+    FirstName: str
+    LastName: str
+    Age: int
+
+@app.post("/students", status_code=201)
+def add_students(new_students: List[Student]):
+    students_db.extend(new_students)
+    return students_db
